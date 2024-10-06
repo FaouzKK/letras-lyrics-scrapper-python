af0e5bdcb8f6d541cf5447ab50624736477f030b
FaouzKK <121231062+FaouzKK@users.noreply.github.com>
2024-10-06 19:22:52 +0000

    Mise a jour

--- app.py
+++ app.py
@@ -74,20 +74,79 @@ def letras_lyrics_manager() :
 
         if lyrics and lyrics.get("success") :
             
-            print("\n")
-            print(colorama.Fore.RED + "─── 「 ✦ ENJOY!! ✦ 」 ──" + colorama.Fore.RESET + "\n")
-            print(colorama.Fore.YELLOW + " Artiste : " + colorama.Fore.RESET + colorama.Fore.GREEN + str(artist) + colorama.Fore.RESET + '\n')
-            print(colorama.Fore.YELLOW + " Chanson : " + colorama.Fore.RESET + colorama.Fore.GREEN +  str(his_song) + colorama.Fore.RESET + "\n")
-            print(colorama.Fore.YELLOW +" Lyrics : " + colorama.Fore.RESET + "\n")
-            print(colorama.Fore.RED + "   ─── ⋆⋅☆⋅⋆ ──" + colorama.Fore.RESET)
-            print("\n")
-            print(lyrics.get("lyrics"))
+            if lyrics.get("lyrics") :
+                print("\n")
+                print(colorama.Fore.RED + "─── 「 ✦ ENJOY!! ✦ 」 ──" + colorama.Fore.RESET + "\n")
+                print(colorama.Fore.YELLOW + " Artiste : " + colorama.Fore.RESET + colorama.Fore.GREEN + str(artist) + colorama.Fore.RESET + '\n')
+                print(colorama.Fore.YELLOW + " Chanson : " + colorama.Fore.RESET + colorama.Fore.GREEN +  str(his_song) + colorama.Fore.RESET + "\n")
+                print(colorama.Fore.YELLOW +" Lyrics : " + colorama.Fore.RESET + "\n")
+                print(colorama.Fore.RED + "   ─── ⋆⋅☆⋅⋆ ──" + colorama.Fore.RESET)
+                print("\n")
+                print(lyrics.get("lyrics"))
+                print("\n\n")
 
-            print("\n\n")
+                goTomain = input(colorama.Fore.GREEN + "Appuyeez entrez pour retourner au menu" + colorama.Fore.RESET)
 
-            goTomain = input(colorama.Fore.GREEN + "Appuyeez entrez pour retourner au menu" + colorama.Fore.RESET)
+                main() 
+            
+            elif lyrics.get("dns") :
+                top_lyrics = letras_scrap_func.song_list(str(lyrics.get("dns")))
+                print("\n\nRecuperation de la top_hit de l'artiste \n")
+                if top_lyrics.get("success") :
+                    array_list = top_lyrics.get("top_hit")
+                    if array_list is not None  and type(array_list) == list :
+                        for i in range(len(array_list)) :
+                            print(f"{i + 1} - {array_list[i].get("name")}")
+                
+                        print("\n\nVeillez choisir un titre ou retourner au menu avec 0")
+                        new_choice = input(colorama.Fore.GREEN +" > " + colorama.Fore.RESET)
 
-            main() 
+                        try :
+                            new_choice = int(new_choice)
+                            if new_choice == 0 :
+                                main()
+
+                            elif new_choice > 0 and new_choice <= len(array_list) :
+                              #  print(f"Je vous choisi {array_list[new_choice - 1].get("href")}")
+
+                                letras_response2 = letras_scrap_func.search_by_link(array_list[new_choice - 1].get("href"))
+
+                                if letras_response2 is not None and letras_response.get("success") :
+                                        
+                                    if letras_response2.get("lyrics") :
+                                        print("\n")
+                                        print(colorama.Fore.RED + "─── 「 ✦ ENJOY!! ✦ 」 ──" + colorama.Fore.RESET + "\n")
+                                        print(colorama.Fore.YELLOW + " Artiste : " + colorama.Fore.RESET + colorama.Fore.GREEN + str(artist) + colorama.Fore.RESET + '\n')
+                                        print(colorama.Fore.YELLOW + " Chanson : " + colorama.Fore.RESET + colorama.Fore.GREEN +  str(array_list[new_choice - 1].get("name")) + colorama.Fore.RESET + "\n")
+                                        print(colorama.Fore.YELLOW +" Lyrics : " + colorama.Fore.RESET + "\n")
+                                        print(colorama.Fore.RED + "   ─── ⋆⋅☆⋅⋆ ──" + colorama.Fore.RESET)
+                                        print("\n")
+                                        print(letras_response2.get("lyrics"))
+                                        print("\n\n")
+
+                                        goTomain = input(colorama.Fore.GREEN + "Appuyeez entrez pour retourner au menu" + colorama.Fore.RESET)
+
+                                        main()
+
+                                    else :
+                                        print("Une erreur inatendue")
+                                        time.sleep(3)
+                                        main()
+                                else :
+                                    print("Une Erreur inatendue")
+                                    time.sleep(3)
+                                    main()
+                                    
+
+                            else :
+                                print("huh") 
+                                time.sleep(3)
+                                main()
+
+                        except Exception as e:
+                            print("Il semble que vous ayez choisi autre qu'un chiffre")
+                            time.sleep(3)
+                            main()
 
         else : 
             print(colorama.Fore.YELLOW +"OUPS ! Erreur lors de l'operation" + colorama.Fore.RESET)
--- letras_scrap_func.py
+++ letras_scrap_func.py
@@ -1,10 +1,10 @@
 import json
 import colorama
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 
-def search_for_lyrics(input : str) -> dict : 
+def search_for_lyrics(input : str) : 
     
     response =  requests.get(f"https://solr.sscdn.co/letras/m1/?q={input}&wt=json&callback=LetrasSug",{})
     
@@ -23,7 +23,7 @@ def search_for_lyrics(input : str) -> dict :
 
         #pprint.pprint(json_content)
 
-        first_content : dict | None = json_content.get("response").get("docs")[0] if len(json_content.get("response").get("docs")) > 0 else None
+        first_content  = json_content.get("response").get("docs")[0] if len(json_content.get("response").get("docs")) > 0 else None
 
         if first_content :
            
@@ -49,7 +49,7 @@ def search_for_lyrics(input : str) -> dict :
          }
 
 
-def get_lyrics(lyric_array : dict) :
+def get_lyrics(lyric_array) :
     
     if not lyric_array.get("success") :
         return {
@@ -80,13 +80,93 @@ def get_lyrics(lyric_array : dict) :
            get_lyrics(lyric_array)
     
     elif lyric_array.get("success") and not lyric_array.get("url") and lyric_array.get("dns") :
-       return {
-          
-       }
+       if lyric_array.get("dns") :
+         return {
+            "success" : True,
+            "dns" : lyric_array.get("dns")
+         }
 
     else :
        return {
           "success" : False 
        }
 
-       
+      
+
+def song_list(dns: str) -> dict[str , bool |  list]:
+   """Fetch Letras dans le cas ou nous avons acces a la page d'un artise
+
+   Args:
+       dns (str): Le dns ou l'identifiant de l'artiste
+
+   Returns:
+       dict[str,list]: Retourne un dictionnaire du top Hit de l'artise
+   """
+   content = requests.get(f"https://www.letras.mus.br/{dns}/")
+
+    # Vérifier si la requête a réussi
+   if content.status_code != 200:
+        print(f"Erreur lors de la récupération de la page : {content.status_code}")
+        return {
+           "success" : False,
+           "top_hit" : []
+        }
+
+   html_text = content.text
+
+    # Correction ici : utiliser class_ au lieu de _class
+   bs = BeautifulSoup(html_text, 'html.parser')
+   song_list_li = bs.find_all('li', class_="songList-table-row --song")
+
+    # Vérifier si l'élément a été trouvé
+   if song_list_li is None or len(song_list_li) == 0:
+      
+      return {
+         "success" : False,
+         "top_hit" : []
+      }
+   
+   top_hit = []
+
+   for li in song_list_li :
+      a_element = li.find("a")
+      if a_element and "href" in a_element.attrs and "title" in a_element.attrs:
+         top_hit.append({
+               "name" : a_element["title"],
+               "href" : "https://www.letras.mus.br" + a_element["href"]
+            })
+
+   #print(top_hit)
+   return {
+      "success" : True,
+      "top_hit" : top_hit
+   }     
+
+
+
+def search_by_link(link : str) :
+        response = requests.get(link)
+
+        content = response.text 
+
+        soup = BeautifulSoup(content,"html.parser")
+
+        lyrics = soup.find(class_="lyric-original")
+
+        if lyrics :
+           lyrics = lyrics.get_text(separator="\n" ,strip=True)
+           
+           return {
+              "success" : True,
+              "lyrics" : lyrics
+           }
+
+        else :
+          return {
+              "success" : False
+           }
+
+
+
+if __name__ == "__main__" :
+   print(search_by_link('https://www.letras.mus.br/franglish/yoyo-petit-coeur/'))
